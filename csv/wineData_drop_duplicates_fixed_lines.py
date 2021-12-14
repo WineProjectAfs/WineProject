07ab886dec312b89c2a96844534ed84c8ba35b72
Blum <robert.blum@accenturefederal.com>
2021-12-14 15:40:57 -0600

    working on removing duplicates

--- csv/wineData_drop_duplicates_fixed_lines.py
+++ csv/wineData_drop_duplicates_fixed_lines.py
@@ -1,10 +1,10 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # wineData_drop_duplicates_fixed_lines.py
 #
-# Input: wineData_fixed_3_broken_lines.csv
+# Input: wineData_fixed_5_broken_lines.csv
 #   Corrected three split lines in original wineData.csv
 #   file (one data line split into two rows):
-#   Rows 40427, 76769, 80904
+#   Rows 40427, 76769, 80904, 92761, 105410
 #
 # Output: wineData_noDups_1.csv
 #   Has duplicate rows removed.
@@ -19,7 +19,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Load the data, and separate the target
-data_path = 'wineData_fixed_3_broken_lines.csv'
+data_path = 'wineData_fixed_5_broken_lines.csv'
 home_data = pd.read_csv(data_path, index_col=0)
  
 # Dup_Rows = home_data[home_data.duplicated()]
